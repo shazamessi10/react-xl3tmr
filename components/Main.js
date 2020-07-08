4d6cafbb131651f56ec95c7d810b9942d8689ed8
shazamessi10 <terapalli.tarunkumar8@gmail.com>
2020-07-07 18:31:59 -0700

    1 binaries uploaded

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+import List from './List'
+import Title from './Title'
+
+class App extends Component {
+ 
+  render() {
+    return(
+      <div>
+       <Title header = {'Photobomb'}/>
+       </div> 
+    )
+  }
+}
+
+export default App
